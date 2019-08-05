e78f2cc7a87f7eac8dcdd4ae894f872172f7ef8f
dan make <danmakes2022@gmail.com>
2019-08-05 12:12:13 -0400

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,16 @@
-import {applyMiddleware, createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
-import {composeWithDevTools} from "redux-devtools-extension";
+import {combineReducers, createStore} from 'redux';
+import {Dishes} from "./dishes";
+import {Comments} from "./comments";
+import {Promotions} from "./promotions";
+import {Leaders} from "./leaders";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
-        composeWithDevTools(
-            applyMiddleware(),
-            // other store enhancers if any
-        )
+    return createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
-
-    return store;
-}
+};
